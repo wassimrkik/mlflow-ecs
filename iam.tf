82d7128bb5a3ed615a3a37aaa63bd11d353b4b7f
Wassim <41171463+wassimrkik@users.noreply.github.com>
2023-12-16 13:39:46 +0400

    added iam permission for ssl key + cert

--- iam.tf
+++ iam.tf
@@ -32,6 +32,8 @@ data "aws_iam_policy_document" "ANP-ML-API-execution" {
     ]
     resources = [
       aws_ssm_parameter.ANP-ML-API-SNOWFLAKE_PASSWORD.arn,
+      aws_ssm_parameter.ANP-ML-API-SSL-CERT,
+      aws_ssm_parameter.ANP-ML-API-SSL-KEY
     ]
   }
 }
