eb5939294cf1d88b0627c996d7ee5e0f799947c0
Wassim <41171463+wassimrkik@users.noreply.github.com>
2024-01-15 15:56:09 +0400

    create ecr for mlflow image + corrected ressource name

--- ecr.tf
+++ ecr.tf
@@ -1,4 +1,4 @@
-resource "aws_ecr_repository" "Anp-UI-FE" {
+resource "aws_ecr_repository" "Anp-API" {
   name                 = "anp/mldev-api"
   image_tag_mutability = "MUTABLE"
   image_scanning_configuration {
@@ -6,7 +6,7 @@ resource "aws_ecr_repository" "Anp-UI-FE" {
   }
 }
 
-resource "aws_ecr_repository" "Anp-UI-FE" {
+resource "aws_ecr_repository" "Anp-API-MLFLOW" {
   name                 = "anp/mlfow"
   image_tag_mutability = "MUTABLE"
   image_scanning_configuration {
