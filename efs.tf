fc9272a7498ea471db05f63e08b740fc89727dc6
Wassim Regaieg <rmwassim@gmail.com>
2024-01-15 23:19:13 +0400

    access index of security group

--- efs.tf
+++ efs.tf
@@ -12,14 +12,14 @@ resource "aws_efs_file_system" "efs_volume" {
 }
 
 resource "aws_efs_mount_target" "ecs_temp_space_az0" {
-  count           = var.deploy_dev_resources && var.ANP-ML-API-ENV == "dev" ? 1 : 0
+  count           = 1
   file_system_id  = aws_efs_file_system.efs_volume.id
   subnet_id       = tolist(data.aws_subnet_ids.subnets.ids)[0]
   security_groups = ["${aws_security_group.ANP-ML-BE-Mlflow[count.index]}"]
 }
 
 resource "aws_efs_mount_target" "ecs_temp_space_az1" {
-  count           = var.deploy_dev_resources && var.ANP-ML-API-ENV == "dev" ? 1 : 0
+  count           = 1
   file_system_id  = aws_efs_file_system.efs_volume.id
   subnet_id       = tolist(data.aws_subnet_ids.subnets.ids)[1]
   security_groups = ["${aws_security_group.ANP-ML-BE-Mlflow[count.index]}"]
@@ -29,7 +29,7 @@ resource "aws_efs_mount_target" "ecs_temp_space_az1" {
 # ECS security-group loop back rule to connect to EFS Volume
 #----------------------------------------
 resource "aws_security_group_rule" "ecs_loopback_rule" {
-  count             = var.deploy_dev_resources && var.ANP-ML-API-ENV == "dev" ? 1 : 0
+  count             = 1
   type              = "ingress"
   from_port         = 0
   to_port           = 0
