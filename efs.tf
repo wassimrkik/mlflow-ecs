2f12884aa75e38054f84127844725c86374cb5ce
Wassim Regaieg <rmwassim@gmail.com>
2024-01-15 23:29:41 +0400

    access index of security group

--- efs.tf
+++ efs.tf
@@ -12,14 +12,14 @@ resource "aws_efs_file_system" "efs_volume" {
 }
 
 resource "aws_efs_mount_target" "ecs_temp_space_az0" {
-  count           = 0
+  count           = 1
   file_system_id  = aws_efs_file_system.efs_volume.id
   subnet_id       = tolist(data.aws_subnet_ids.subnets.ids)[0]
   security_groups = ["${aws_security_group.ANP-ML-BE-Mlflow[count.index]}"]
 }
 
 resource "aws_efs_mount_target" "ecs_temp_space_az1" {
-  count           = 0
+  count           = 1
   file_system_id  = aws_efs_file_system.efs_volume.id
   subnet_id       = tolist(data.aws_subnet_ids.subnets.ids)[1]
   security_groups = ["${aws_security_group.ANP-ML-BE-Mlflow[count.index]}"]
@@ -33,6 +33,17 @@ resource "aws_security_group_rule" "ecs_loopback_rule" {
   type              = "ingress"
   from_port         = 0
   to_port           = 0
+  protocol          = "-1"
+  self              = true
+  description       = "Loopback"
+  security_group_id = aws_security_group.ANP-ML-BE-Mlflow[count.index]
+}
+
+resource "aws_security_group_rule" "ecs_loopback_rule2" {
+  count             = 0
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 2049
   protocol          = "-1"
   self              = true
   description       = "Loopback"
