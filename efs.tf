9b9d9d87fac120d1b6b9f60e68e86bd3a96913aa
Wassim Regaieg <rmwassim@gmail.com>
2024-01-15 23:38:31 +0400

    added id to security group

--- efs.tf
+++ efs.tf
@@ -14,13 +14,13 @@ resource "aws_efs_file_system" "efs_volume" {
 resource "aws_efs_mount_target" "ecs_temp_space_az0" {
   file_system_id  = aws_efs_file_system.efs_volume.id
   subnet_id       = tolist(data.aws_subnet_ids.subnets.ids)[0]
-  security_groups = ["${aws_security_group.ANP-ML-BE-Mlflow}"]
+  security_groups = ["${aws_security_group.ANP-ML-BE-Mlflow.id}"]
 }
 
 resource "aws_efs_mount_target" "ecs_temp_space_az1" {
   file_system_id  = aws_efs_file_system.efs_volume.id
   subnet_id       = tolist(data.aws_subnet_ids.subnets.ids)[1]
-  security_groups = ["${aws_security_group.ANP-ML-BE-Mlflow}"]
+  security_groups = ["${aws_security_group.ANP-ML-BE-Mlflow.id}"]
 }
 
 #----------------------------------------
@@ -33,7 +33,7 @@ resource "aws_security_group_rule" "ecs_loopback_rule" {
   protocol          = "-1"
   self              = true
   description       = "Loopback"
-  security_group_id = aws_security_group.ANP-ML-BE-Mlflow
+  security_group_id = aws_security_group.ANP-ML-BE-Mlflow.id
 }
 
 resource "aws_security_group_rule" "ecs_loopback_rule2" {
@@ -43,5 +43,5 @@ resource "aws_security_group_rule" "ecs_loopback_rule2" {
   protocol          = "-1"
   self              = true
   description       = "Loopback"
-  security_group_id = aws_security_group.ANP-ML-BE-Mlflow
+  security_group_id = aws_security_group.ANP-ML-BE-Mlflow.id
 }
