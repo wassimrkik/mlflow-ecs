242fb99d7b7917a3426a26fdee51188c77d3a3c2
Wassim Regaieg <rmwassim@gmail.com>
2024-01-16 18:40:31 +0400

    modified self sg rule

--- ecs_mlflow.tf
+++ ecs_mlflow.tf
@@ -90,11 +90,11 @@ resource "aws_security_group" "ANP-ML-BE-Mlflow" {
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = -1
-    self        = true
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port = 0
+    to_port   = 0
+    protocol  = -1
+    self      = true
+    #cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
     from_port   = 0
