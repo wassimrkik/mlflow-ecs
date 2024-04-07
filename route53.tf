40dfbfbd6649d98d8258f95f7f568c66841d9726
Wassim <41171463+wassimrkik@users.noreply.github.com>
2024-04-07 14:33:13 +0400

    dns dynamic entries

--- route53.tf
+++ route53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "ANP-ML-API" {
   zone_id = data.aws_route53_zone.selected.zone_id
-  name    = "anp-api-mldev"
+  name    = var.dns_name
   type    = "CNAME"
   ttl     = "300"
   records = [aws_lb.ANP-ML-API.dns_name]
