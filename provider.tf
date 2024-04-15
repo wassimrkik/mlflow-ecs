8c180560e48729242ca5fc469b071c6dedaede15
Wassim <41171463+wassimrkik@users.noreply.github.com>
2024-04-15 11:39:28 +0400

    adding APP_GEN1 tag for cost allocation

--- provider.tf
+++ provider.tf
@@ -11,6 +11,7 @@ provider "aws" {
   region = var.awsregion
   default_tags {
     tags = {
+      APP_GEN1          = "AnP Spend allocation"
       Project           = "AnP"
       Owner             = "CHC"
       CE_Application_ID = "APM0069253"
